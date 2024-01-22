54bc68394e461a99b010e98e88440ce7bae296b9
Miraitowa_zcx <2038322151@qq.com>
2024-01-22 21:10:54 +0800

    2024.1.22-9:10

--- app/src/main/java/com/eduos/application/MainActivity.java
+++ app/src/main/java/com/eduos/application/MainActivity.java
@@ -1,10 +1,9 @@
 package com.eduos.application;
 
 import android.content.Intent;
-import android.view.View;
+import android.os.Bundle;
 import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import com.eduos.application.activity.LoginActivity;
 import com.eduos.application.activity.RegisterActivity;
 
