23abe0b0cea976df2b892e88b13dfad27f94a34a
Miraitowa_zcx <2038322151@qq.com>
2024-01-22 20:29:43 +0800

    2024.1.22-8:29

--- app/src/main/java/com/eduos/application/MainActivity.java
+++ app/src/main/java/com/eduos/application/MainActivity.java
@@ -1,16 +1,34 @@
 package com.eduos.application;
 
+import android.content.Intent;
+import android.view.View;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.eduos.application.activity.LoginActivity;
 
 /**
+ * 启动界面
+ *
  * @author 20383
  */
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnRegister;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btnLogin = (Button) findViewById(R.id.login_btn);
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
